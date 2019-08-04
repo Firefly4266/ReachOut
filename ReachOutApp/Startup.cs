1610f42d05a7e852c596def7fd1c61a751df01e2
Firefly4266 <gaines.johnp@gmail.com>
2019-08-03 19:34:02 -0700

    initial commit

---
+++ ReachOutApp/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ReachOutApp.Startup))]
+namespace ReachOutApp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
